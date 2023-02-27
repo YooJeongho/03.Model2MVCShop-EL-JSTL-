03e4798dd5130c5063fc53189a351da7a00377ae
Bitcamp <Bitcamp@DESKTOP-IR25RL3>
2023-02-27 17:11:37 +0900

    master로 잘못입력해뜸

--- 03.Model2MVCShop(EL,JSTL)/src/main/java/com/model2/mvc/service/domain/Product.java
+++ 03.Model2MVCShop(EL,JSTL)/src/main/java/com/model2/mvc/service/domain/Product.java
@@ -75,6 +75,6 @@ public class Product {
 	public String toString() {
 		return "ProductVO : [fileName]" + fileName
 				+ "[manuDate]" + manuDate+ "[price]" + price + "[prodDetail]" + prodDetail
-				+ "[prodName]" + prodName + "[prodNo]" + prodNo;
+				+ "[prodName]" + prodName + "[prodNo]" + prodNo+"[TranCode] : "+proTranCode;
 	}	
 }
